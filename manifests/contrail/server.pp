3a7fccafd1b63129b0cb19616e98393963971201
hkumarmk <hkumarmk@gmail.com>
2015-04-08 02:29:48 +0530

    more changes
    Service blocker did not work as packages installed in node initialization phase
    and for some reason ordering with service blocker did not work.

--- manifests/contrail/server.pp
+++ manifests/contrail/server.pp
@@ -25,5 +25,16 @@ class rjil::contrail::server () {
   Rjil::Service_blocker['zookeeper'] -> Package['contrail-dns']
   Rjil::Service_blocker['zookeeper'] -> Package['contrail-analytics']
 
+
+  Package['zookeeper'] -> Package['contrail-config-openstack']
+  Package['zookeeper'] -> Package['contrail-control']
+  Package['zookeeper'] -> Package['contrail-dns']
+  Package['zookeeper'] -> Package['contrail-analytics']
+
+  Package['dsc'] -> Package['contrail-config-openstack']
+  Package['dsc'] -> Package['contrail-control']
+  Package['dsc'] -> Package['contrail-dns']
+  Package['dsc'] -> Package['contrail-analytics']
+
   include ::contrail
 }
